db7aff4eeac491029e7496dbd63bbfe45318ae40
ch <mkmsacc@outlook.com>
2022-09-24 10:18:44 +0700

    Optimize imports

--- src/test/java/com/example/demo/StudentRepositoryTest.java
+++ src/test/java/com/example/demo/StudentRepositoryTest.java
@@ -1,12 +1,11 @@
 package com.example.demo;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 class StudentRepositoryTest {
